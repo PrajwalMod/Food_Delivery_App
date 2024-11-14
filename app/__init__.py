2ed61dc878f86f4cbae5d57723f7878683cef138
Prithviraj Dey <prithviraj.dey95@gmail.com>
2024-11-15 03:02:09 +0530

    Adding Migration Files and other minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -7,21 +7,24 @@ from app.routes.restaurant_routes import restaurant_bp
 
 from flask_migrate import Migrate
 from app.database import db
+import os
 
 migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
-    # app.config['DATABASE'] = MongoClient('mongodb://localhost:27017/')['food_delivery_app']
+
+    app.config['SECRET_KEY'] = os.urandom(24)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/food_delivery_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    swagger = Swagger(app)
+
     app.register_blueprint(user_bp, url_prefix='/api/users')
     app.register_blueprint(order_bp, url_prefix='/api/orders')
     app.register_blueprint(restaurant_bp, url_prefix='/api/restaurants')
 
+    # Initialize database and Swagger
+    Swagger(app)
     db.init_app(app)
     migrate.init_app(app, db)
 
